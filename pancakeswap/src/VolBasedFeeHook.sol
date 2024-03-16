812be73a6d4a24e8af7334c6a61056a530809ecf
jpw993 <jpw993@gmail.com>
2024-03-16 16:18:33 +0000

    add getFeeImpl

--- pancakeswap/src/VolBasedFeeHook.sol
+++ pancakeswap/src/VolBasedFeeHook.sol
@@ -12,6 +12,8 @@ import {CLBaseHook} from "./pool-cl/CLBaseHook.sol";
 contract VolBasedFeeHook is CLBaseHook, ICLDynamicFeeManager {
     using PoolIdLibrary for PoolKey;
 
+    uint256 constant MIN_FEE = 5000; // 0.005%
+
     constructor(ICLPoolManager _poolManager) CLBaseHook(_poolManager) {}
 
     function getHooksRegistrationBitmap() external pure override returns (uint16) {
@@ -44,5 +46,28 @@ contract VolBasedFeeHook is CLBaseHook, ICLDynamicFeeManager {
 
     function getFee(address sender, PoolKey calldata key) external view override returns (uint24) {
         return 500;
+    }
+
+    function getFeeImpl(uint256 amt0, uint256 amt1, uint256 volatility, bool tradeReducesGap)
+        internal
+        pure
+        returns (uint256 fee)
+    {
+        uint256 volume;
+        if (amt0 > 0) {
+            volume = amt0;
+        } else {
+            volume = amt1;
+        }
+
+        uint256 volume_factor = 5000; // 0.005%
+        uint256 volatility_factor = 2700; // 0.0027%
+        uint256 trade_reduces_gap_factor = 1;
+        fee = MIN_FEE + (volume_factor * (volume ** 15000000) * (volatility_factor * (volatility ** 20000000)));
+        // We reduce the fee for when trades bring us further from market because those tend to be more uninformed traders
+        if (!tradeReducesGap) {
+            fee *= trade_reduces_gap_factor;
+        }
+        return fee;
     }
 }
